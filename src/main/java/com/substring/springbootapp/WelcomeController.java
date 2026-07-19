a0ab9b6fc3a703e05ac01a794a650123c64b2cf6
Your Name <your-email@example.com>
2026-07-20 05:26:52 +0530

    workflow/users endpoint added

--- src/main/java/com/substring/springbootapp/WelcomeController.java
+++ src/main/java/com/substring/springbootapp/WelcomeController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/welcome")
 public class WelcomeController {
 
     @GetMapping("/users")
