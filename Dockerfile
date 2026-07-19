11fed86278309e1f37990bd613f48325bab77f0a
Subhash Kumar Jha <subhaashjha@gmail.com>
2026-07-19 14:15:03 +0000

    Initial commit - Spring Boot project

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+From eclipse-temurin:21-jre
+
+WORKDIR /app
+
+COPY target/*.jar app.jar
+
+EXPOSE 8081
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/substring/springbootapp/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.substring.springbootapp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Welcome to Spring Boot API!");
+    }
+}
